4e42b497f62573a36d05a714bb061c1575fef430
Ninjawe1 <4hmedxp7@gmail.com>
2025-10-21 19:30:39 +0300

    C12

--- client/src/components/payroll-dialog.tsx
+++ client/src/components/payroll-dialog.tsx
@@ -30,6 +30,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { isUnauthorizedError } from "@/lib/authUtils";
+import { apiRequest } from "@/lib/queryClient";
 
 interface PayrollDialogProps {
   open: boolean;
